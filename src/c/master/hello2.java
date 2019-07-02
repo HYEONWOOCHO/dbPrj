538c8400a755dc13956e01e4dd5cd06b3e6f7603
HYEONWOOCHO <i9096055@naver.com>
2019-07-02 13:33:25 +0900

    s5 helloMater

--- src/c/master/hello2.java
+++
@@ -1,9 +0,0 @@
-package c.master;
-
-public class hello2 {
-
-	public hello2() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
---
+++ src/c/mater/HelloMaster.java
@@ -0,0 +1,8 @@
+package c.mater;
+
+public class HelloMaster {
+
+	public static void main(String[] args) {
+		System.out.println("hello master");
+	}
+}
